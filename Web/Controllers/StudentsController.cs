f6c5bd787646e50ff56c17323fe12a1ae0402a71
Pablo Navarro <5821512-navarropablojavier@users.noreply.gitlab.com>
2020-06-21 19:33:18 -0300

    feat(actSpef): add controllers de teachers and students, GET, POST, PUT DELETE LOGIC

--- Web/Controllers/StudentsController.cs
+++ Web/Controllers/StudentsController.cs
@@ -21,5 +21,59 @@ namespace Web.Controllers
         {
             this.context = context;
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<StudentsIdentity>>> Get()
+        {
+            return await context.Students.ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "Student")]
+        public async Task<ActionResult<StudentsIdentity>> Get(int id)
+        {
+            var Student = await context.Students.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+            return Student;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] StudentsIdentity Student)
+        {
+            context.Add(Student);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("Student", new { id = Student.Id }, Student);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put(int id, [FromBody] StudentsIdentity Value)
+        {
+            if (id != Value.Id)
+            {
+                return BadRequest();
+            }
+            context.Entry(Value).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult<StudentsIdentity> Delete(int id)
+        {
+            var Student = context.Students.FirstOrDefault(x => x.Id == id);
+
+            if (Student == null)
+            {
+                return NotFound();
+            }
+
+            context.Remove(Student);
+            context.SaveChanges();
+            return Ok(Student);
+        }
     }
 }
